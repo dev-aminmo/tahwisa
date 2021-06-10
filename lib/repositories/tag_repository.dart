58a5a90bbc3bda77cc198c542dce624558fdde9c
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-06-10 11:33:54 +0100

    query tags

--- lib/repositories/tag_repository.dart
+++ lib/repositories/tag_repository.dart
@@ -5,20 +5,16 @@ import 'api/api_endpoints.dart';
 
 class TagRepository {
   /// Mocks fetching Tags from network API with delay of 500ms.
-  bool _fetched = false;
-  List<Tag> tags = [];
 
   Future<List<Tag>> getTags(String query) async {
-    //TODO query tags
-    if (!_fetched) {
-      _fetched = true;
-      var response = await Dio().get(Api.tags);
-      print(response.data);
-      var data = response.data;
-      for (var jsonTag in data) {
-        var tag = Tag.fromJson(jsonTag);
-        tags.add(tag);
-      }
+    var response = await Dio().get(Api.tags + "?query=$query");
+    print(response.data);
+    var data = response.data;
+    List<Tag> tags = [];
+
+    for (var jsonTag in data) {
+      var tag = Tag.fromJson(jsonTag);
+      tags.add(tag);
     }
 
     return tags
