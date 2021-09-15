2a20f61f00bc51a1d47a302dbdc80b1ea44bc114
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-09-16 00:20:31 +0100

    Place detail screen carousel

--- lib/screens/profile/views/place_details.dart
+++ lib/screens/profile/views/place_details.dart
@@ -1,7 +1,10 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tahwisa/repositories/models/place.dart';
+import 'package:tahwisa/style/my_colors.dart';
 
-class PlaceDetailsScreen extends StatelessWidget {
+class PlaceDetailsScreen extends StatefulWidget {
   static const String routeName = '/place_details';
 
   static Route route({@required Place place}) {
@@ -18,35 +21,74 @@ class PlaceDetailsScreen extends StatelessWidget {
   });
 
   @override
+  _PlaceDetailsScreenState createState() => _PlaceDetailsScreenState();
+}
+
+class _PlaceDetailsScreenState extends State<PlaceDetailsScreen> {
+  var currentPosition = 0;
+  @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      extendBodyBehindAppBar: true,
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.vertical(
-                  bottom:
-                      Radius.elliptical(MediaQuery.of(context).size.width, 50),
-                ),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                    place.pictures[0],
-                  ),
-                ),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          backwardsCompatibility: false,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.light,
+          ),
         ),
-      ),
-    );
+        extendBodyBehindAppBar: true,
+        backgroundColor: MyColors.white,
+        body: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            CarouselSlider.builder(
+                options: CarouselOptions(
+                    scrollPhysics: BouncingScrollPhysics(),
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 2),
+                    disableCenter: false,
+                    enableInfiniteScroll: false,
+                    aspectRatio: 1.1,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        currentPosition = index;
+                      });
+                    },
+                    viewportFraction: 1),
+                itemCount: widget.place.pictures.length,
+                itemBuilder: (context, index, realIndex) {
+                  return SizedBox(
+                    width: double.infinity,
+                    child: Image.network(
+                      widget.place.pictures[index].replaceFirstMapped(
+                          "image/upload/",
+                          (match) =>
+                              "image/upload/w_${(width).round()},f_auto/"),
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: widget.place.pictures.map((picture) {
+                int index = widget.place.pictures.indexOf(picture);
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: currentPosition == index
+                        ? Color.fromRGBO(0, 0, 0, 0.9)
+                        : Color.fromRGBO(0, 0, 0, 0.4),
+                  ),
+                );
+              }).toList(),
+            ),
+          ]),
+        ));
   }
 }
