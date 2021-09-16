bd81fb3eb46a28056bd1d1d87222c209aa4ecac3
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-09-16 01:17:46 +0100

    Place detail full screen image preview

--- lib/screens/profile/views/place_details.dart
+++ lib/screens/profile/views/place_details.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:photo_view/photo_view.dart';
 import 'package:tahwisa/repositories/models/place.dart';
 import 'package:tahwisa/style/my_colors.dart';
 
@@ -29,6 +30,7 @@ class _PlaceDetailsScreenState extends State<PlaceDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
@@ -60,14 +62,36 @@ class _PlaceDetailsScreenState extends State<PlaceDetailsScreen> {
                     viewportFraction: 1),
                 itemCount: widget.place.pictures.length,
                 itemBuilder: (context, index, realIndex) {
-                  return SizedBox(
-                    width: double.infinity,
-                    child: Image.network(
-                      widget.place.pictures[index].replaceFirstMapped(
-                          "image/upload/",
-                          (match) =>
-                              "image/upload/w_${(width).round()},f_auto/"),
-                      fit: BoxFit.cover,
+                  return GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FullScreenPage(
+                                  child: PhotoView(
+                                imageProvider:
+                                    NetworkImage(widget.place.pictures[index]),
+                              )
+                                  /*Image.network(
+                                  widget.place.pictures[index],
+                                  */ /*.replaceFirstMapped(
+                                      "image/upload/",
+                                      (match) =>
+                                          "image/upload/w_${(width).round()},h_${(height).round()},f_auto/"),*/ /*
+                                  //       fit: BoxFit.cover,
+                                  fit: BoxFit.none,
+                                ),*/
+
+                                  )),
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Image.network(
+                        widget.place.pictures[index].replaceFirstMapped(
+                            "image/upload/",
+                            (match) =>
+                                "image/upload/w_${(width).round()},f_auto/"),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   );
                 }),
@@ -90,5 +114,56 @@ class _PlaceDetailsScreenState extends State<PlaceDetailsScreen> {
             ),
           ]),
         ));
+  }
+}
+
+class FullScreenPage extends StatefulWidget {
+  FullScreenPage({
+    @required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  _FullScreenPageState createState() => _FullScreenPageState();
+}
+
+class _FullScreenPageState extends State<FullScreenPage> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        backwardsCompatibility: false,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light,
+        ),
+      ),
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
+      body: AnimatedPositioned(
+        duration: Duration(milliseconds: 333),
+        curve: Curves.fastOutSlowIn,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: widget.child,
+      ),
+    );
   }
 }
