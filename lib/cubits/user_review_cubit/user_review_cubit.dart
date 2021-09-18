95a8a05c1f620e497ec24623c298dcca6630d76b
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-09-18 12:14:52 +0100

    user review cubit structure

--- lib/cubits/user_review_cubit/user_review_cubit.dart
+++ lib/cubits/user_review_cubit/user_review_cubit.dart
@@ -2,9 +2,19 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:tahwisa/repositories/models/Review.dart';
+import 'package:tahwisa/repositories/review_repository.dart';
 
 part 'user_review_state.dart';
 
 class UserReviewCubit extends Cubit<UserReviewState> {
-  UserReviewCubit() : super(UserReviewInitial());
+  final ReviewRepository repository;
+  final placeID;
+
+  UserReviewCubit({@required this.repository, @required this.placeID})
+      : super(UserReviewLoading()) {
+    fetchUserReview(placeID);
+  }
+  void fetchUserReview(var placeID) {}
+  void updateReview(var reviewID) {}
+  void deleteReview(var reviewID) {}
 }
